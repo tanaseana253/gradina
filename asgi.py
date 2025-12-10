735c57eecfadfded5cf23246a19e29dd54955115
tanaseana253 <tanase_ana253@yahoo.com>
2025-12-10 17:46:00 +0200

    Remove duplicate gitignore and fix merge conflicts

--- asgi.py
+++ asgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 ASGI config for gradinaCraciun project.
 
@@ -15,21 +14,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gradinaCraciun.settings')
 
 application = get_asgi_application()
-=======
-"""
-ASGI config for gradinaCraciun project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gradinaCraciun.settings')
-
-application = get_asgi_application()
->>>>>>> ead77dd6054e37580d97b52cacd1e7d99f236219
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 WSGI config for gradinaCraciun project.
 
@@ -15,21 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gradinaCraciun.settings')
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for gradinaCraciun project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gradinaCraciun.settings')
-
-application = get_wsgi_application()
->>>>>>> ead77dd6054e37580d97b52cacd1e7d99f236219
