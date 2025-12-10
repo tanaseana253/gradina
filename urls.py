ddd37410d4bc80ea86d46b922c439cc91267f9dd
tanaseana253 <tanase_ana253@yahoo.com>
2025-12-10 17:46:00 +0200

    Remove duplicate gitignore and fix merge conflicts

--- urls.py
+++ urls.py
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
 """
 URL configuration for gradinaCraciun project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
 from django.contrib import admin
@@ -25,53 +11,19 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls')),
-    path("store/", include("store.urls")),  # new
-    path('accounts/', include("django.contrib.auth.urls")),  # new
-    path('store/', include("django.contrib.auth.urls")),
+
+    # Store app URLs
+    path('', include('store.urls')),  # main store routes
+
+    # Authentication
+    path('accounts/', include("django.contrib.auth.urls")),
+
+    # Home page fallback
     path("", TemplateView.as_view(template_name="product_list.html"), name="product_list"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
+# Serve media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-"""
-URL configuration for gradinaCraciun project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-
-from gradinaCraciun import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('store.urls')),
-    path("store/", include("store.urls")),  # new
-    path('accounts/', include("django.contrib.auth.urls")),  # new
-    path('store/', include("django.contrib.auth.urls")),
-    path("", TemplateView.as_view(template_name="product_list.html"), name="product_list"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-# Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> ead77dd6054e37580d97b52cacd1e7d99f236219
